a35a9ef027c8b326ee41a78d98d571ffa0168c9b
Alexis Castro Puse <castropuse@gmail.com>
2020-06-09 23:15:10 -0500

    mapa terminado

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 
 const tilesProvider = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
-let myMap = L.map('my-map').setView();
+let myMap = L.map('my-map').setView([-6.774766, -79.837137], 17);
+
+L.tileLayer(tilesProvider, {
+    maxZoom: 20,
+}).addTo(myMap);
+
+let marker = L.marker([-6.774766, -79.837137]).addTo(myMap);
+marker.bindPopup('<b>Barbería Navajas</b>').openPopup();
