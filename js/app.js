dc4278f068ddc3a15fd1d678d982ea25c88a6909
Alexis Castro Puse <castropuse@gmail.com>
2020-06-24 00:11:54 -0500

    agregando hover

--- js/app.js
+++ js/app.js
@@ -4,8 +4,11 @@ const tilesProvider = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 let myMap = L.map('my-map').setView([-6.774766, -79.837137], 17);
 
 L.tileLayer(tilesProvider, {
-    maxZoom: 20,
+    maxZoom: 22,
 }).addTo(myMap);
 
-let marker = L.marker([-6.774766, -79.837137]).addTo(myMap);
-marker.bindPopup('<b>Barbería Navajas</b>').openPopup();
+let enero = L.marker([-6.774766, -79.837137]).addTo(myMap);
+let bolognesi = L.marker([-6.776215, -79.833376]).addTo(myMap);
+
+bolognesi.bindPopup('<b>Barbería Navajas</b>').openPopup();
+enero.bindPopup('<b>Barbería Navajas</b>').openPopup();
